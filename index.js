95ea7360158d3830bbc1bd0da8c01fe35e5bb57a
ipatalas <fido@list.pl>
2020-04-13 20:02:58 +0200

    Fix ESLint issues

--- index.js
+++ index.js
@@ -5,39 +5,39 @@ const log = require('elastic-logger')('uptime-mqtt');
 const MQTT_URL = process.env.MQTT_URL;
 
 if (!MQTT_URL) {
-	log.error('Missing MQTT_URL environment variable');
-	process.exit(1);
+    log.error('Missing MQTT_URL environment variable');
+    process.exit(1);
 }
 
 const client = MQTT.connect(MQTT_URL);
 
 client.once('connect', async () => {
-	log.info('Connected to ' + MQTT_URL);
+    log.info('Connected to ' + MQTT_URL);
 
-	const zeroPad = value => (value < 10) ? '0' + value : value.toString();
+    const zeroPad = value => (value < 10) ? '0' + value : value.toString();
 
-	const uptime = os.uptime();
-	const days = Math.floor(uptime / (60 * 60 * 24));
-	const hours = Math.floor(uptime / (60 * 60)) % 24;
-	const minutes = Math.floor(uptime / 60) % 60;
-	const text = `${days} days, ${zeroPad(hours)}:${zeroPad(minutes)}`;
+    const uptime = os.uptime();
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const hours = Math.floor(uptime / (60 * 60)) % 24;
+    const minutes = Math.floor(uptime / 60) % 60;
+    const text = `${days} days, ${zeroPad(hours)}:${zeroPad(minutes)}`;
 
-	try {
-		const options = { retain: true };
+    try {
+        const options = { retain: true };
 
-		await Promise.all([
-			client.publish('home/uptime/total_seconds', uptime.toString(), options),
-			client.publish('home/uptime/days', days.toString(), options),
-			client.publish('home/uptime/hours', hours.toString(), options),
-			client.publish('home/uptime/minutes', minutes.toString(), options),
-			client.publish('home/uptime/text', text, options)
-		]);
+        await Promise.all([
+            client.publish('home/uptime/total_seconds', uptime.toString(), options),
+            client.publish('home/uptime/days', days.toString(), options),
+            client.publish('home/uptime/hours', hours.toString(), options),
+            client.publish('home/uptime/minutes', minutes.toString(), options),
+            client.publish('home/uptime/text', text, options)
+        ]);
 
-		await client.end();
+        await client.end();
 
-		log.info({ uptime: text }, 'Finished');
-	} catch (error) {
-		log.error(error);
-		process.exit(1);
-	}
+        log.info({ uptime: text }, 'Finished');
+    } catch (error) {
+        log.error(error);
+        process.exit(1);
+    }
 });
